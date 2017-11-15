b4bc9b66222b6a5cd8c2c2635c790a4147bec667
Adland Lee <adland.lee@gmail.com>
2017-11-14 23:08:10 -0800

    add initial data stub

---
+++ iOS-Experiments/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  iOS-Experiments
+//
+//  Created by Adland Lee on 11/14/17.
+//  Copyright © 2017 4MFD. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
